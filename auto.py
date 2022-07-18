24477f9d97a0c9062f18eed6751abf3a7b471a02
Leon <62799947+leon4464@users.noreply.github.com>
2022-07-18 19:11:10 +0200

    Added GUI to auto.py allowing for custom filepaths

--- auto.py
+++ auto.py
@@ -1,6 +1,117 @@
-import time
+import time, os
+from tkinter import *
+from tkinter import filedialog
+import webbrowser
 
 initialtime = time.time()
+
+def setupGUI():
+    global l1ent, l2ent, l3ent, l4ent, l5ent, l6ent, l7ent, teamname, open_after, data, root
+    l1ent = l2ent = l3ent = l4ent = l5ent = l6ent = l7ent = open_after = data = None
+    root = Tk()
+    root.title("Tower 3D Log Grader V0.2.0")
+    x = 500
+    y = 800
+    root.geometry(str(x) + "x" + str(y))
+    root.resizable(False, False)
+    root.configure(background="white")
+    title_preset = ("Calibri", 18, "bold")
+    subtitle_preset = ("Calibri", 15, "italic")
+    body_preset = ("Calibri", 11,)
+    button_preset = ("Calibri", 10,)
+    num_rows = 9
+    teamname = "default" # not yet implemented
+
+    class uploadButton():
+        def __init__(self, root, x, y, row, column, font, preferredname):
+            self.root = root
+            self.x = X
+            self.y = y
+            self.color = "red"
+            self.row = row
+            self.column = column
+            self.font = font
+            self.button = Button(root, bg=self.color, text="Click to browse... ", command=self.openfile, font=self.font)
+            self.button.grid(row=self.row, column=0)
+            self.path = None
+            self.preferredname = preferredname
+
+        def openfile(self):
+            file = filedialog.askopenfile(mode='r')
+            if file:
+                self.path = os.path.abspath(file.name)
+                if self.preferredname in self.path:
+                    self.color = "green"
+                else:
+                    self.color = "orange"
+                self.label = self.path.split("\\")[-1]
+                self.rebuild()
+
+        def rebuild(self):
+            self.button.destroy()
+            self.button = Button(self.root, bg=self.color, text=self.label, command=self.openfile)
+            self.button.grid(row=self.row, column=0)
+        
+        def getPath(self):
+            return self.path
+
+    l0 = Label(root, text="Configuration", background="white", font=title_preset)
+    l0.grid(row = 0, column = 0, sticky = W, pady = 2)
+
+    l1 = Label(root, text="Terminal Synonyms: ", background="white", font=subtitle_preset)
+    l1.grid(row = 1, column = 0, sticky = W, pady = 2)
+    l1sub = Label(root, text="Comma separated synonyms for terminal ex: TERMINAL, RAMP, APRON", background="white", font=body_preset)
+    l1sub.grid(row = 2, column = 0, sticky = W, pady = 2)
+    l1ent = Entry(root)
+    l1ent.insert(0, "TERMINAL, APRON, RAMP")
+    l1ent.grid(row=3, column=0)
+
+    l2 = Label(root, text="Banned Runways: ", background="white", font=subtitle_preset)
+    l2.grid(row = 4, column = 0, sticky = W, pady = 2)
+    l2sub = Label(root, text="Comma separated runways ex: 24R, 25R", background="white", font=body_preset)
+    l2sub.grid(row = 5, column = 0, sticky = W, pady = 2)
+    l2ent = Entry(root)
+    l2ent.grid(row=6, column=0)
+
+    l3 = Label(root, text="Airport code ex: LAX: ", background="white", font=subtitle_preset)
+    l3.grid(row = 7, column = 0, sticky = W, pady = 2)
+    l3sub = Label(root, text="3 letter IATA Airport code ex: LAX ", background="white", font=body_preset)
+    l3sub.grid(row = 8, column = 0, sticky = W, pady = 2)
+    l3ent = Entry(root)
+    l3ent.grid(row=9, column=0)
+    
+    l4 = Label(root, text="Airlines file (????_airlines.txt) ", background="white", font=subtitle_preset)
+    l4.grid(row = 10, column = 0, sticky = W, pady = 2)
+    l4ent = uploadButton(root, 0, 0, 11, 0, button_preset, "_airlines")
+
+    l5 = Label(root, text="General Aviation Schedule (????_gaandlocaltraffic.txt) ", background="white", font=subtitle_preset)
+    l5.grid(row = 13, column = 0, sticky = W, pady = 2)
+    l5ent = uploadButton(root, 0, 0, 14, 0, button_preset, "_gaandlocaltraffic")
+
+    l6 = Label(root, text="Commercial Schedule (????_schedule.txt) ", background="white", font=subtitle_preset)
+    l6.grid(row = 15, column = 0, sticky = W, pady = 2)
+    l6ent = uploadButton(root, 0, 0, 16, 0, button_preset, "_schedule")
+
+    l7 = Label(root, text="Output Log (output_log.txt) ", background="white", font=subtitle_preset)
+    l7.grid(row = 18, column = 0, sticky = W, pady = 2)
+    l7ent = uploadButton(root, 0, 0, 19, 0, button_preset, "output_log")
+    
+    # unimplemented checkbox option to open the file after analysis completion
+    """l8 = Label(root, text="Other configs: ", background="white", font=subtitle_preset)
+    l8.grid(row = 20, column = 0, sticky = W, pady = 2)
+    l8ent = Checkbutton(root, text="Open file after completion", background="white", variable=open_after, onvalue=True, offvalue=False)
+    l8ent.grid(row=21, column=0)"""
+
+    def endGUI():
+        global l1ent, l2ent, l3ent, l4ent, l5ent, l6ent, l7ent, teamname, open_after, data, root
+        data = l1ent.get().split(", "), l2ent.get().split(", "), l3ent.get(), l4ent.getPath(), l5ent.getPath(), l6ent.getPath(), l7ent.getPath(), teamname, open_after
+        root.destroy()
+    
+    lf = Button(root, command=endGUI, text="Start", background="blue", foreground="white")
+    lf.grid(row=22, column=0)
+
+    root.mainloop()
+
 
 def importAirlines(filename, commentprefix):
     airlines = {}
@@ -165,16 +276,19 @@ def calculateHandledAircraft(logged_instructions, GA_departures, GA_arrivals, de
 
 if __name__ == "__main__":
     # Configuration
-    # Change as needed depending on Tower!3D Pro version and tournament conditions
-    terminal_synonyms = ["TERMINAL", "RAMP", "APRON"] # possible names for taxiing to final spot
-    banned_runways = ["24R"] # list of runways disallowed for departure / arrivals
-    arrivalairportcode = "LAX" # airport code without kilo
-    airlines_filename = "klax_airlines.txt" 
-    GA_filename = "klax_gaandlocaltraffic.txt"
-    commercial_filename = "klax_schedule.txt"
-    gamelog_filename = "output_log.txt"
-    teamname = "default"
-    output_file = open(teamname + "-" + arrivalairportcode + "-auto.log", "x")
+    setupGUI()
+    terminal_synonyms = data[0]
+    banned_runways = data[1]
+    arrivalairportcode = data[2]
+    airlines_filename = data[3]
+    GA_filename = data[4]
+    commercial_filename = data[5]
+    gamelog_filename = data[6]
+    teamname = data[7]
+    open_after = data[8]
+
+    output_file_path = teamname + "-" + arrivalairportcode + "-auto.log"
+    output_file = open(output_file_path, "x")
 
     # Importing Airline Data as a dictionary (Tower!3D Pro encodes AAL as AA for example for American Airlines)
     # Usage: "AA" key (weird game format) / index will return "AAL" (ICAO)
@@ -254,11 +368,14 @@ if __name__ == "__main__":
     departure_message = "Commercial - departures: "
     departure_runwaykeys = getDictKeysList(departure_stats)
     remainder = departure_count
-    for i in range(0, len(departure_runwaykeys)):
-        departure_message += "RWY" + departure_runwaykeys[i] + " - " + str(departure_stats[departure_runwaykeys[i]]) + "/" + str(departure_count) + " (" + str((departure_stats[departure_runwaykeys[i]]/departure_count)*100)[0:6] + "%) "
-        remainder -= int(departure_stats[departure_runwaykeys[i]])
-    departure_message += "Unhandled: " + str(remainder) + " (" + str(remainder / departure_count)[0:6] + "%)"
-    output_file.write(departure_message + "\n")
+    try:
+        for i in range(0, len(departure_runwaykeys)):
+            departure_message += "RWY" + departure_runwaykeys[i] + " - " + str(departure_stats[departure_runwaykeys[i]]) + "/" + str(departure_count) + " (" + str((departure_stats[departure_runwaykeys[i]]/departure_count)*100)[0:6] + "%) "
+            remainder -= int(departure_stats[departure_runwaykeys[i]])
+        departure_message += "Unhandled: " + str(remainder) + " (" + str(remainder / departure_count)[0:6] + "%)"
+        output_file.write(departure_message + "\n")
+    except:
+        pass
 
     arrival_message = "Commercial - arrivals: "
     arrival_runwaykeys = getDictKeysList(arrival_stats)
@@ -276,3 +393,7 @@ if __name__ == "__main__":
     output_file.write("Go arounds: " + str(go_arounds) + "\n")
     output_file.write("\n")
     output_file.write("Total Runtime (ms): " + str((time.time() - initialtime) * 1000)[:6] + "\n")
+
+    print(open_after)
+    if open_after:
+        webbrowser.open(output_file)
